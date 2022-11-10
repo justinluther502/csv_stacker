8ec997c6ea4836678ab61490390a088ef6a0486e
Justin Luther <justinluther@gmail.com>
2022-11-10 10:19:11 -0500

    ready to build main stacker fn.

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,38 @@
 use polars::prelude::*;
-
-mod config;
+use std::{
+    fs::{read_dir, File},
+    io::{prelude::*, BufReader},
+};
 
 pub fn read_single_csv(filepath: &str) -> Result<DataFrame, PolarsError> {
-    let colnames = config::lines_from_file("colnames.txt");
-    let df = LazyCsvReader::new(filepath)
+    Ok(LazyCsvReader::new(filepath)
         .has_header(true)
         .finish()?
         .collect()
-        .unwrap();
-    df.select(colnames)
+        .unwrap())
+}
+
+pub fn get_colnames(filename: &str) -> Vec<String> {
+    let file = File::open(filename).expect("couldn't find colnames file.");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line in colnames file."))
+        .collect()
+}
+
+pub fn get_csv_filenames(csv_dir_path: &str) -> Vec<String> {
+    read_dir(csv_dir_path)
+        .expect("Couln't read CSV directory")
+        .map(|f| f.unwrap().path().to_str().unwrap().to_owned())
+        .collect()
+}
+
+pub fn stack_csvs(csvs: &Vec<String>, colnames: &Vec<String>, out: &str) -> () {
+    for csv in csvs {
+        println!("{}", csv);
+    }
+    println!("{:?}", colnames);
+    println!("{}", out);
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use csv_stacker::read_single_csv;
+use csv_stacker::{get_colnames, get_csv_filenames, stack_csvs};
+use std::env;
 
 fn main() {
-    let df = read_single_csv("csvs/foo.csv").unwrap();
-    println!("{}", df);
+    let args: Vec<String> = env::args().collect();
+    let csv_dir_path = &args[1];
+    let output_path = &args[2];
+    let filenames = get_csv_filenames(&csv_dir_path);
+    let colnames = get_colnames("colnames.txt");
+    stack_csvs(&filenames, &colnames, &output_path);
 }
